1293eeee65aa406a835bc6fcb6fc4893aa8af5f6
Lukas Witzani <witzan02@stud.uni-passau.de>
2015-11-26 12:59:48 +0100

    LWI: zweite activity mit hot-cold-view

--- schnitzel-client/app/src/main/java/de/iteratec/schnitzel/client/SchnitzelMainActivity.java
+++ schnitzel-client/app/src/main/java/de/iteratec/schnitzel/client/SchnitzelMainActivity.java
@@ -1,13 +1,14 @@
 package de.iteratec.schnitzel.client;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.concurrent.ExecutionException;
 
@@ -63,16 +64,8 @@ public class SchnitzelMainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        Intent intent = new Intent(this, NexttargetActivity.class);
+        startActivity(intent);
         return super.onOptionsItemSelected(item);
     }
 }
